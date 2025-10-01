0d7740391b62c28bc67a1807770ab34dfe5b2cde
Paula <paula1122pmail.com>
2025-10-01 19:11:45 +0200

    Dateien neu Strukturieren

--- main.py
+++ main.py
@@ -1,92 +1,19 @@
-# hier ein neues Pythonspiel uwu
-# diesmal 4 Gewinnt
-# Objektorientiert
 
-# coole Darstellung über Zeilenumbruch?
-
-
-#Herzstück oder so
-AusgesuchteSpalte = int(input("wohin? "))       # wichtig
-    # ist ein Literal und somit seltsam
-
-
-
-
-# Logik Spielbrett
-def LogikSpielbrett(zeile, spalte):
-    """ hier wird geschaut, ob der Eintrag im Feld schon getätigt wurde bzw. wird das Eingegebene verarbeitet """
     
-            # counter um zu wissen, welche Spalten für die Zeile schon besetzt sind
-    counter_0 = 0
-    counter_1 = 0
-    counter_2 = 0
-    counter_3 = 0
-    zeile1 = zeile
-    spalte1 = spalte
-    
-        
-    spielbrett_logik = []
-    for m in range(zeile1):
+
+def Spielbrett(zeile, spalte):
+    """ Diese Funktion erstellt das Spielbrett. Hierbei werden mehrere Listen in eine Liste 
+    zusammengefügt, um eine Matrix zu erstellen. """
+    for m in range(zeile):
         zeile = []
-        for n in range(spalte1):
-            zeile.append(0)             # Spielbrett mit 0 füllen 
-        spielbrett_logik.append(zeile)
-            
-    while counter_2 < 3:
-            
-        if AusgesuchteSpalte == 0:
-            counter_0 += 1
-            spielbrett_logik[counter_0][AusgesuchteSpalte] == 1
-        if AusgesuchteSpalte == 1:
-            counter_1 += 1
-            spielbrett_logik[counter_1][AusgesuchteSpalte] == 1
-        if AusgesuchteSpalte == 2:
-            counter_2 += 1
-            spielbrett_logik[counter_2][AusgesuchteSpalte] == 1
-        if AusgesuchteSpalte == 3:
-            counter_3 += 1    
-            spielbrett_logik[counter_3][AusgesuchteSpalte] == 1
-            
-        print("ausgewähltes: ")
-        print("0: ", counter_0)
-        print("1: ", counter_1)
-        print("2: ", counter_2)
-        print("3: ", counter_3)
-        
-    
-        #LogikSpielbrett(zeile_test, spalte_test)
-        # Endlosschleife
-    
-
-
-
-# Spielbrett für die Ausgabe
-spielbrett = []
-zeile = 4
-spalte = 4
-for m in range(zeile):
-    zeile = []
-    for n in range(spalte):
-        zeile.append(" ")             # Spielbrett mit leeren Einträgen füllen      # av, vllt. Leerzeichen in ASCII?
-    spielbrett.append(zeile)
-    
-
-
-
-
-# Eingabe in Spielbrett einarbeiten
-        # --> Logik Spielbrett reyclen?
-if AusgesuchteSpalte < len(spielbrett[0]):      # av, später muss sich die Zeile variable mit ändern
-    spielbrett[0][AusgesuchteSpalte] = "x"      # av, später ausählbar
-else:
-    print("so viele Spalten gibt es heute nicht")
-    
-
-
+        for n in range(spalte):
+            zeile.append(" ")             # Spielbrett mit leeren Einträgen füllen      # av, vllt. Leerzeichen in ASCII?
+        spielbrett.append(zeile)
     
 
 def Ausgabe(zeile, spalte):
-    """ Spielbrett "hübsch" ausgeben lassen """     #av
+    """ In dieser Funktion wird das Spielbrett für die Konsole formatiert ausgegeben. 
+    Dazu wird die Spielbrett Matrix durchgegenagen, wobei für jeden Eintrag ein leeres Feld eingefügt wird. """
     print("0     1     2     3")    # av
     for i in range(zeile):
         for j in range(spalte):
@@ -94,14 +21,115 @@ def Ausgabe(zeile, spalte):
         print()     # printe nach jedem vollendeten j sozusagen einen Zeilenumbruch
         
 
+
+class Spieler(object):
+    
+    def __init__(self, icon = str):
+        self.icon = icon            
+        
+
+
+
+def Abfrage(zeile, spieler):
+    """ beschreiben """
+    while True:
+        try:
+            value = int(input('Spalte eingeben: ')) # versuche, die Benutzereingabe in eine Ganzzahl umzuwandeln, könnte fehlschlagen
+            global counter 
+            global col0
+            global col1
+            global col2
+            global col3
+            counter = counter+1
+            if value == 0:
+                if spielbrett_logik[col0][counter] == True:
+                    col0 = col0 +1
+            elif value == 1:
+                col1 = col1 +1
+            elif value == 2:
+                col2 = col2 +1            
+            elif value == 3:
+                col3 = col3 +1 
+            else:
+                return          # ergibt das Sinn? av        wichtig          
+            
+            
+            if value < zeile:
+                spielbrett[0][value] = spieler
+                
+                
+            if spielbrett_logik[col0][value] == True:
+                spielbrett[col0][value] = spieler
+            else:
+                print("passt nicht")
+        except:
+            continue # versuche es erneut, gehe zum Anfang der Schleife
+        break # verlässt die while-Schleife, Wert definitiv eine Ganzzahl
+    
+    
+
+def SpielbrettLogik(zeile, spalte):
+    """beschreiben"""
+    for m in range(zeile):
+        zeile = []
+        for n in range(spalte):
+            zeile.append(False)             # Spielbrett mit False Einträgen füllen
+        spielbrett_logik.append(zeile)
+    
+def AbfrageLogik():
+    if spielbrett[col0][counter] == "x":
+        spielbrett_logik[col0][counter] == True
+    if spielbrett[col1][counter] == "x":
+        spielbrett_logik[col1][counter] == True
+    if spielbrett[col2][counter] == "x":
+        spielbrett_logik[col2][counter] == True
+    if spielbrett[col3][counter] == "x":
+        spielbrett_logik[col3][counter] == True
+
+
         
         
-# sowas wie meine main()
+# int main()
+# {
+
+""" Settings für Matrix etc. """
+spielbrett = []
+spielbrett_logik = []
 zeile = 4
 spalte = 4
-logik = LogikSpielbrett(zeile, spalte)
-ausgeben = Ausgabe(zeile, spalte)   
 
-#input("mach mal noch was: ")
+counter = 0
+col0 = 0
+col1 = 0
+col2 = 0
+col3 = 0
 
-# return 0;         :P
+""" Funktionen aufrufen """
+board = Spielbrett(zeile, spalte)   #ab hier ist Spielbrett befüllt
+logic_board = SpielbrettLogik(zeile, spalte)
+
+""" Spieler """
+player1 = Spieler("x")
+player2 = Spieler("o")
+
+""" Spielschleife """
+ausgeben = Ausgabe(zeile, spalte) 
+while counter < 4:
+    if (counter % 2) == 0:
+        abfrage = Abfrage(zeile, player1.icon)
+        ausgeben = Ausgabe(zeile, spalte) 
+    elif (counter % 2) == 1:
+        print("ich bin ungerade")
+        abfrage = Abfrage(zeile, player2.icon)
+        ausgeben = Ausgabe(zeile, spalte) 
+
+"""Testbereich"""
+
+
+
+# return 0; 
+# }
+
+# :)
+
+
