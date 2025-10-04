81cfe2b75a55305346bf62526046343881d9877f
Paula <paula1122pmail.com>
2025-10-04 21:11:45 +0200

    objektorientierte Erweiterung

--- main.py
+++ main.py
@@ -24,111 +24,113 @@ class AutoSpieler:
         
 
 
-
-def Spielbrett(zeile, spalte):
-    """ Diese Funktion erstellt das Spielbrett. Hierbei werden mehrere Listen in eine Liste 
-    zusammengefügt, um eine Matrix zu erstellen. """
-    for m in range(row_num):
-        zeile = []
-        for n in range(col_num):
-            zeile.append(" ")             # Spielbrett mit leeren Einträgen füllen      # av, vllt. Leerzeichen in ASCII?
-        spielbrett.append(zeile)
+class VierGewinnt():
     
-
-def Ausgabe(row_num, col_num):
-    """ In dieser Funktion wird das Spielbrett für die Konsole formatiert ausgegeben. 
-    Dazu wird die Spielbrett Matrix durchgegenagen, wobei für jeden Eintrag ein leeres Feld eingefügt wird. """
-    print("0     1     2     3     4     5     6")    # av
-    for i in range(row_num):
-        for j in range(col_num):
-            print(spielbrett[row_num-i-1][j], end="  |  ")   # end bedeutet am Ende jedes Eintrages
-        print()     # printe nach jedem vollendeten j sozusagen einen Zeilenumbruch
-
-
-def Abfrage(zeile, spieler):
-    """ beschreiben """
-    while True:             # SR
-        try:
-            value = int(input('Spalte eingeben: ')) # Benutzereingabe in Ganzzahl
-            global counter 
-            global col0
-            global col1
-            global col2
-            global col3
-            global col4
-            global col5
-            global col6
-            counter = counter +1
-            
-            """ Gedächtinis über die Züge: """
-            if value < zeile:   # Hier wird gegengespüft, ob die eingegebene Zahl auch innerhalb des Spielfeldes liegt
-                if value == 0:
-                    spielbrett[col0][value] = spieler   # hier wird das gewünschte Spieler-Symbol genutzt
-                    col0 = col0 +1
-                    break
-                if value == 1:
-                    spielbrett[col1][value] = spieler
-                    col1 = col1 +1
-                    break
-                if value == 2:
-                    spielbrett[col2][value] = spieler
-                    col2 = col2 +1
-                    break         
-                if value == 3:
-                    spielbrett[col3][value] = spieler
-                    col3 = col3 +1      
-                    break   
-                if value == 4:
-                    spielbrett[col4][value] = spieler
-                    col4 = col4 +1    
-                    break
-                if value == 5:
-                    spielbrett[col5][value] = spieler
-                    col5 = col5 +1    
-                    break
-                if value == 6:
-                    spielbrett[col6][value] = spieler
-                    col6 = col6 +1    
-                    break  
- 
-        except:
-            continue # versuche es erneut, gehe zum Anfang der Schleife
+    def __init__(self, spielbrett, row_num, col_num, win = False):
+        self.spielbrett = spielbrett
+        self.row_num = row_num
+        self.col_num = col_num
+        self.win = win 
     
-    
-    
+    def Spielbrett(self):
+        """ Diese Funktion erstellt das Spielbrett. Hierbei werden mehrere Listen in eine Liste 
+        zusammengefügt, um eine Matrix zu erstellen. """
+        for m in range(self.row_num):
+            zeile = []
+            for n in range(self.col_num):
+                zeile.append(" ")             # Spielbrett mit leeren Einträgen füllen      # av, vllt. Leerzeichen in ASCII?
+            spielbrett.append(zeile)
 
+    def Ausgabe(self):
+        """ In dieser Funktion wird das Spielbrett für die Konsole formatiert ausgegeben. 
+        Dazu wird die Spielbrett Matrix durchgegenagen, wobei für jeden Eintrag ein leeres Feld eingefügt wird. """
+        print("0     1     2     3     4     5     6")    # av
+        for i in range(self.row_num):
+            for j in range(self.col_num):
+                print(spielbrett[self.row_num-i-1][j], end="  |  ")   # end bedeutet am Ende jedes Eintrages
+            print()     # printe nach jedem vollendeten j sozusagen einen Zeilenumbruch
 
-def checkWin() -> bool:
+    def Abfrage(self, spieler):
+        """ beschreiben """
+        while True:             # SR
+            try:
+                value = int(input('Spalte eingeben: ')) # Benutzereingabe in Ganzzahl
+                global counter 
+                global col0
+                global col1
+                global col2
+                global col3
+                global col4
+                global col5
+                global col6
+                counter = counter +1
+
+                """ Gedächtinis über die Züge: """
+                if value < self.row_num:   # Hier wird gegengespüft, ob die eingegebene Zahl auch innerhalb des Spielfeldes liegt
+                    if value == 0:
+                        spielbrett[col0][value] = spieler   # hier wird das gewünschte Spieler-Symbol genutzt
+                        col0 = col0 +1
+                        break
+                    if value == 1:
+                        spielbrett[col1][value] = spieler
+                        col1 = col1 +1
+                        break
+                    if value == 2:
+                        spielbrett[col2][value] = spieler
+                        col2 = col2 +1
+                        break         
+                    if value == 3:
+                        spielbrett[col3][value] = spieler
+                        col3 = col3 +1      
+                        break   
+                    if value == 4:
+                        spielbrett[col4][value] = spieler
+                        col4 = col4 +1    
+                        break
+                    if value == 5:
+                        spielbrett[col5][value] = spieler
+                        col5 = col5 +1    
+                        break
+                    if value == 6:
+                        spielbrett[col6][value] = spieler
+                        col6 = col6 +1    
+                        break  
+                    
+            except:
+                continue # versuche es erneut, gehe zum Anfang der Schleife
+             
+
+    def checkWin(self) -> bool:
+        """ beschreiben """
+        global win 
+        """ horizontal """
+        for col in range(self.col_num-3):        # -3 aufgrund der range
+            for row in range(self.row_num):
+                if(spielbrett[row][col] == spielbrett[row][col+1] == spielbrett[row][col+2] == spielbrett[row][col+3] != " "):
+                    print("Gewonnen")
+                    return True
+
+        """ vertikal """
+        for col in range(self.col_num -3):        
+            for row in range(self.row_num -3):
+                if(spielbrett[row][col] == spielbrett[row+1][col] == spielbrett[row+2][col] == spielbrett[row+3][col] != " "):
+                    print("Gewonnen")
+                    return True
     
-    global win 
-    """ horizontal """
-    for col in range(col_num-3):        # -3 aufgrund der range   SR
-        for row in range(row_num):
-            if(spielbrett[row][col] == spielbrett[row][col+1] == spielbrett[row][col+2] == spielbrett[row][col+3] != " "):
-                print("Gewonnen")
-                return True
-    
-    """ vertikal """
-    for col in range(col_num -3):        
-        for row in range(row_num -3):
-            if(spielbrett[row][col] == spielbrett[row+1][col] == spielbrett[row+2][col] == spielbrett[row+3][col] != " "):
-                print("Gewonnen")
-                return True
- 
-    """ diagonal hochzu """
-    for col in range(col_num -3):       
-        for row in range(row_num -3):
-            if(spielbrett[row][col] == spielbrett[row+1][col+1] == spielbrett[row+2][col+2] == spielbrett[row+3][col+3] != " "):
-                print("Gewonnen")  
-                return True
-                
-    """ diagonal herunter """
-    for col in range(col_num):        # SR
-        for row in range(row_num -3):
-            if(spielbrett[row][col] == spielbrett[row+1][col-1] == spielbrett[row+2][col-2] == spielbrett[row+3][col-3] != " "):
-                print("Gewonnen") 
-                return True
-    return False
+        """ diagonal hochzu """
+        for col in range(self.col_num -3):       
+            for row in range(self.row_num -3):
+                if(spielbrett[row][col] == spielbrett[row+1][col+1] == spielbrett[row+2][col+2] == spielbrett[row+3][col+3] != " "):
+                    print("Gewonnen")  
+                    return True
+
+        """ diagonal herunter """
+        for col in range(self.col_num):        # SR
+            for row in range(self.row_num -3):
+                if(spielbrett[row][col] == spielbrett[row+1][col-1] == spielbrett[row+2][col-2] == spielbrett[row+3][col-3] != " "):
+                    print("Gewonnen") 
+                    return True
+        return False
     
 
         
@@ -151,26 +153,25 @@ col4 = 0
 col5 = 0
 col6 = 0
 
-""" Funktionen aufrufen """
-board = Spielbrett(row_num, col_num)   #ab hier ist Spielbrett befüllt
-
-""" Spieler """
+""" Instanzen """
+vier_gewinnt = VierGewinnt(spielbrett, row_num, col_num)
 player1 = Spieler("x")
 player2 = Spieler("o")
 
 
 """ Spielschleife """
+board = vier_gewinnt.Spielbrett()        # Erstellt die Matrix die als Spielbrett genutzt wird
+ausgeben = vier_gewinnt.Ausgabe() 
 
 while win != True:
-    ausgeben = Ausgabe(row_num, col_num) 
     if (counter % 2) == 0:
-        abfrage = Abfrage(row_num, player1.icon)
-        ausgeben = Ausgabe(row_num, col_num) 
-        win = checkWin()
+        abfrage = vier_gewinnt.Abfrage(player1.icon)
+        ausgeben = vier_gewinnt.Ausgabe() 
+        win = vier_gewinnt.checkWin()
     elif (counter % 2) == 1:
-        abfrage = Abfrage(row_num, player2.icon)
-        ausgeben = Ausgabe(row_num, col_num) 
-        win = checkWin()
+        abfrage = vier_gewinnt.Abfrage(player2.icon)
+        ausgeben = vier_gewinnt.Ausgabe() 
+        win = vier_gewinnt.checkWin()
 
 """Testbereich"""
 
--- testen.py
+++ testen.py
@@ -1,26 +1,15 @@
-mat = [[0,0],[0,0]]
-mat [1][1] = 1
 
-print(mat)
+"""
+print("sag was: ")
+input = input()                # nicht int input führt zu Fehlern
+if (input in ("0", "1", "2", "3")):
+    #spielbrett[0][1] = x
+    input = int(input)
+    print("passt")
+else:
+    print("passt nicht")
+    
+print(input)
+"""
 
 
-#print("sag was: ")
-#input = input()                # nicht int input führt zu Fehlern
-#if (input == ("0" or "1" or "2" or "3")):
-#    #spielbrett[0][1] = x
-#    input = int(input)
-#    print("passt")
-#else:
-#    print("passt nicht")
-    
-    
-    
-class Spieler(object):
-    
-    def __init__(self, icon = str):
-        self.icon = icon            # zweites icon bezieht sich auf Attribut der Methode
-        
-p1 = Spieler("x")
-p2 = Spieler("o")
-
-print(p1.icon)
